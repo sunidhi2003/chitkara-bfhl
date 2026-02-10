a5fd5129d1d06fb62260e77acd795013c2c76cbf
sunidhi2003 <sunidhiagg7@gmail.com>
2026-02-10 12:15:56 +0530

    files add

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 const OFFICIAL_EMAIL = "sunidhi1300.be23@chitkara.edu.in";
 
 /* ---------------- HEALTH API ---------------- */
